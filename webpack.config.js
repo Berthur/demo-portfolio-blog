7915c503c5b4624f17de8f67f5ebe8fd091027ff
Berthur <jondenvise@gmail.com>
2025-08-07 22:36:06 +0300

    Refactoring: Skip ts/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: {
-    main: "./ts/src/main.ts",
+    main: "./src/main.ts",
   },
   output: {
     path: path.resolve(__dirname, 'build'),
