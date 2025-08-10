beb613afbce3084a8e3f888b04e6978da2037969
Berthur <jondenvise@gmail.com>
2025-08-11 00:52:04 +0300

    Add Katex macros

--- src/latex.ts
+++ src/latex.ts
@@ -3,8 +3,16 @@ import katex from "katex";
 export class LatexElement extends HTMLElement {
     constructor() {
         super();
-        katex.render(this.innerText, this);
+        katex.render(this.innerText, this, LatexElement.options);
     }
+
+    static readonly options = {
+        //displayMode: true,
+        macros: {
+            '\\R': '\\mathbb{R}',
+            '\\norm': '\\|#1\\|',
+        },
+    };
 }
 
 customElements.define('span-latex', LatexElement);
