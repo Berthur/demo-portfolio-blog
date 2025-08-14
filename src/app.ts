5093972f7f649814d3b01e433152fb51817b8825
Berthur <jondenvise@gmail.com>
2025-08-14 17:37:18 +0300

    Auto-generated next and previous links for blog entries

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,77 @@
 import { FractalDemo } from "./demos/fractalDemo";
 import { ParticlesDemo } from "./demos/particlesDemo";
 import { TrafficDemo } from "./demos/trafficDemo";
+import * as siteJSON from "../html/site.json";
+
+interface BlogEntry {
+    index: number;
+    title: string;
+    path: string;
+    date: string;
+}
 
 export class App {
+    siteJSON = siteJSON;
+    private blogEntries = new Map<string, BlogEntry>();
+
     demos = Object.seal({
         particles: ParticlesDemo,
         fractal: FractalDemo,
         traffic: TrafficDemo,
     });
+
+    constructor() {
+        let i=0;
+        for (const be of this.siteJSON.blog.entries) {
+            const entry: BlogEntry = Object.assign({ index: i++ }, be);
+            this.blogEntries.set(be.path, entry);
+        }
+    }
+
+    populateBlogLinks(): void {
+        const blogPath = location.pathname.slice(1);
+        const currEntry = this.getBlogEntry(blogPath);
+        const prevEntry = this.getPrevBlogEntry(currEntry);
+        const nextEntry = this.getNextBlogEntry(currEntry);        
+
+        const prevLink = document.createElement('div');
+        if (prevEntry) {
+            const link = document.createElement('a');
+            link.innerText = "Prev: " + prevEntry.title;
+            link.href = prevEntry.path;
+            prevLink.append(link);
+        }
+
+        const nextLink = document.createElement('div');
+        if (nextEntry) {
+            const link = document.createElement('a');
+            link.innerText = "Next: " + nextEntry.title;
+            link.href = nextEntry.path;
+            nextLink.append(link);
+        }
+
+        const linkContainers = document.getElementsByClassName('blogLinks');
+        for (let i=0; i<linkContainers.length; ++i) {
+            linkContainers[i].append(prevLink.cloneNode(true));
+            linkContainers[i].append(nextLink.cloneNode(true));
+        }
+    }
+
+    getBlogEntry(path: string): BlogEntry {
+        const entry = this.blogEntries.get(path);
+        if (!entry) throw new Error(`Could not find blog entry '${ path }'`);
+        return entry;
+    }
+
+    getPrevBlogEntry(currEntry: BlogEntry): BlogEntry | undefined {
+        const prevJSON = this.siteJSON.blog.entries[currEntry.index - 1];
+        if (prevJSON) return this.blogEntries.get(prevJSON.path);
+        else return undefined;
+    }
+
+    getNextBlogEntry(currEntry: BlogEntry): BlogEntry | undefined {
+        const nextJSON = this.siteJSON.blog.entries[currEntry.index + 1];
+        if (nextJSON) return this.blogEntries.get(nextJSON.path);
+        else return undefined;
+    }
 }
