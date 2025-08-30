792e90fce5788b14e72494bd13296aeb2d4bd4c9
Berthur <jondenvise@gmail.com>
2025-08-30 23:55:05 +0300

    Collapse sidebar when clicking outside it

--- html/scripts.js
+++ html/scripts.js
@@ -3,6 +3,18 @@
  * General scripts needed for the website navigation to function
  */
 
-document.getElementById('sidebarButton')?.addEventListener('click', e => {
-    document.getElementById('sidebar')?.classList?.toggle('active');
+const sidebarButton = document.getElementById('sidebarButton');
+const sidebarOverlay = document.getElementById('sidebarOverlay');
+const sidebar = document.getElementById('sidebar');
+
+sidebarButton?.addEventListener('click', e => {
+    sidebarOverlay?.classList?.toggle('active');
+});
+
+sidebarOverlay?.addEventListener('click', e => {
+    sidebarOverlay?.classList?.remove('active');
+});
+
+sidebar?.addEventListener('click', e => {
+    e.stopPropagation();
 });
