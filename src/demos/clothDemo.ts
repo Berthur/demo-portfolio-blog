d9935015a175792f0b282e0a58207cd9c0123c07
Berthur <jondenvise@gmail.com>
2025-10-25 01:20:41 +0300

    Cloth demo: Use multiple render targets to store position and velocity in separate textures

--- src/demos/clothDemo.ts
+++ src/demos/clothDemo.ts
@@ -1,4 +1,4 @@
-import { DataTexture, DoubleSide, FloatType, GLSL3, Mesh, PerspectiveCamera, PlaneGeometry, RGBAFormat, Scene, ShaderMaterial, Uniform, Vector2, WebGLRenderer, WebGLRenderTarget } from "three";
+import { DataTexture, DoubleSide, FloatType, GLSL3, Mesh, PerspectiveCamera, PlaneGeometry, RGBAFormat, Scene, ShaderMaterial, Texture, Uniform, Vector2, WebGLRenderer, WebGLRenderTarget } from "three";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
@@ -14,8 +14,6 @@ export class ClothDemo extends Demo {
 
     private readonly canvas: HTMLCanvasElement;
     private readonly renderer: WebGLRenderer;
-    private currStateTarget: WebGLRenderTarget;
-    private nextStateTarget: WebGLRenderTarget;
     private readonly scene: Scene;
     private readonly camera: PerspectiveCamera;
     private readonly mesh: Mesh;
@@ -23,8 +21,12 @@ export class ClothDemo extends Demo {
     private readonly computePass: ShaderPass;
     private readonly composer: EffectComposer;
 
-    private texture1: DataTexture;
-    private texture2: DataTexture;
+    private currStateTarget: WebGLRenderTarget;
+    private nextStateTarget: WebGLRenderTarget;
+    private positionTexture0: Texture;
+    private positionTexture1: Texture;
+    private velocityTexture0: Texture;
+    private velocityTexture1: Texture;
 
     constructor(container: HTMLElement) {
         super(container);
@@ -36,8 +38,8 @@ export class ClothDemo extends Demo {
         });
 
         this.renderer = new WebGLRenderer({ canvas: this.canvas });
-        this.currStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
-        this.nextStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
+        this.currStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y, { count: 2 });
+        this.nextStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y, { count: 2 });
 
         this.scene = new Scene();
         this.camera = new PerspectiveCamera(90);
@@ -47,8 +49,10 @@ export class ClothDemo extends Demo {
 
         this.initializeTextures();
 
-        this.computePass = new ShaderPass(ComputeShader, 'prevState');
+        this.computePass = new ShaderPass(ComputeShader, 'positionTexture');
+        this.computePass.material.glslVersion = GLSL3;
         this.computePass.uniforms.mousePos = new Uniform(this.mousePos);
+
         const renderPass = new RenderPass(this.scene, this.camera);
         renderPass.renderToScreen = true;
         this.composer = new EffectComposer(this.renderer);
@@ -67,7 +71,7 @@ export class ClothDemo extends Demo {
         });
         this.material.uniforms.viewSize = new Uniform(this.dimensions);
         this.material.uniforms.clothDimensions = new Uniform(this.clothDimensions);
-        this.material.uniforms.currState = new Uniform(null);
+        this.material.uniforms.positionTexture = new Uniform(null);
         this.material.uniforms.delta = new Uniform(0);
 
         this.mesh = new Mesh();
@@ -95,20 +99,30 @@ export class ClothDemo extends Demo {
             data[4*i + 1] = 2 * ~~(i / this.clothDimensions.x) / this.clothDimensions.y - 1;
         }
 
-        this.texture1 = new DataTexture(data, this.clothDimensions.x, this.clothDimensions.y);
-        this.texture1.type = FloatType;
-        this.texture1.format = RGBAFormat;
-        this.texture1.needsUpdate = true;
+        this.positionTexture0 = new DataTexture(data, this.clothDimensions.x, this.clothDimensions.y);
+        this.positionTexture0.type = FloatType;
+        this.positionTexture0.format = RGBAFormat;
+        this.positionTexture0.needsUpdate = true;
 
-        this.texture2 = this.texture1.clone();
+        this.positionTexture1 = this.createEmptyTexture();
+        this.velocityTexture0 = this.createEmptyTexture();
+        this.velocityTexture1 = this.createEmptyTexture();
 
-        this.currStateTarget.texture = this.texture1;
-        this.nextStateTarget.texture = this.texture2;
+        this.currStateTarget.textures = [this.positionTexture1, this.velocityTexture1];
+        this.nextStateTarget.textures = [this.positionTexture0, this.velocityTexture0];
+    }
+
+    private createEmptyTexture(): Texture {
+        const t = new Texture({ width: this.clothDimensions.x, height: this.clothDimensions.y } as any);
+        t.type = FloatType;
+        t.format = RGBAFormat;
+        return t;
     }
 
     private updateUniforms(delta: number): void {
         this.computePass.uniforms.delta.value = delta;
-        this.material.uniforms.currState.value = this.nextStateTarget.texture;
+        this.computePass.uniforms.velocityTexture.value = this.currStateTarget.textures[1];
+        this.material.uniforms.positionTexture.value = this.nextStateTarget.textures[0];
     }
 
     start(): void {
@@ -116,15 +130,14 @@ export class ClothDemo extends Demo {
     }
 
     restart(): void {
-        console.log("Restarting!");
+        console.warn("Restarting!");
 
-        this.texture1.dispose();
-        this.texture2.dispose();
-        this.currStateTarget.dispose();
-        this.nextStateTarget.dispose();
-        this.currStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
-        this.nextStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
-        this.initializeTextures();
+        // TODO: Dispose of textures
+        // this.currStateTarget.dispose();
+        // this.nextStateTarget.dispose();
+        // this.currStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
+        // this.nextStateTarget = new WebGLRenderTarget(this.clothDimensions.x, this.clothDimensions.y);
+        // this.initializeTextures();
 
         // TODO: Restart mesh geometry
     }
@@ -168,8 +181,9 @@ const ComputeShader = {
         viewSize: { value: new Vector2(1, 1) },
         delta: { value: 0 },
         damping: { value: 1 },
-        prevState: { value: null },
         clothDimensions: { value: new Vector2(1, 1) },
+        positionTexture: { value: null },
+        velocityTexture: { value: null },
     },
     defines: {
         BORDERS_REFLECT: false,
@@ -191,7 +205,11 @@ const ComputeShader = {
         uniform float delta;
         uniform float damping;
         uniform vec2 mousePos;
-        uniform sampler2D prevState;
+        uniform sampler2D positionTexture;
+        uniform sampler2D velocityTexture;
+
+        layout(location = 0) out vec4 outPosition;
+        layout(location = 1) out vec4 outVelocity;
 
         void main() {
             ivec2 fragCoord = ivec2(gl_FragCoord.xy);
@@ -201,36 +219,38 @@ const ComputeShader = {
             corrMousePos.y = -corrMousePos.y;
             corrMousePos /= maxViewSize;
 
-            vec4 state = texelFetch(prevState, fragCoord, 0);
-            vec2 p = state.rg + vec2(0.0);
-            vec2 v = state.ba;
+            vec2 p = texelFetch(positionTexture, fragCoord, 0).rg;
+            vec2 v = texelFetch(velocityTexture, fragCoord, 0).rg;
             float d = 0.001 * delta;
 
-            vec2 f = vec2(0.0, -0.3);
+            // Hang from upper corners:
+            if (!(fragCoord.y == clothDimensions.y - 1 && (fragCoord.x == 0 || fragCoord.x == clothDimensions.x - 1))) {
 
-            for (int j=-1; j<=1; ++j) for (int i=-1; i<=1; ++i) if (!(i == 0 && j == 0)) {
-                ivec2 coords = fragCoord + ivec2(i, j);
-                if (coords.x >= 0 && coords.x < clothDimensions.x && coords.y >= 0 && coords.y < clothDimensions.y) {
-                    vec4 state2 = texelFetch(prevState, coords, 0);
-                    vec2 dir = state2.rg - p;
-                    float r = length(dir);
-                    dir /= r;
-                    float restR = 0.2;
-                    float k = 20.0;
-                    //if (bool(i ^ j)) restR *= SQRT_2;
-                    f += k * (r - restR) * dir;
+                vec2 f = vec2(0.0, -0.3);
+
+                for (int j=-1; j<=1; ++j) for (int i=-1; i<=1; ++i) if (!(i == 0 && j == 0)) {
+                    ivec2 coords = fragCoord + ivec2(i, j);
+                    if (coords.x >= 0 && coords.x < clothDimensions.x && coords.y >= 0 && coords.y < clothDimensions.y) {
+                        vec2 p1 = texelFetch(positionTexture, coords, 0).rg;
+                        vec2 dir = p1 - p;
+                        float r = length(dir);
+                        dir /= r;
+                        float restR = 0.2;
+                        float k = 20.0;
+                        //if (bool(i ^ j)) restR *= SQRT_2;
+                        f += k * (r - restR) * dir;
+                    }
                 }
+
+                vec2 a = f;
+                v += d * a;
+                v *= max(0.5, (1.0 - d * damping));
+                p += d * v;
+
             }
 
-            vec2 a = f;
-            v += d * a;
-            v *= max(0.5, (1.0 - d * damping));
-            p += d * v;
-
-            // Hang from upper corners:
-            if (fragCoord.y == clothDimensions.y - 1 && (fragCoord.x == 0 || fragCoord.x == clothDimensions.x - 1)) p = state.rg;
-
-            gl_FragColor = vec4(p, v);
+            outPosition = vec4(p, v);
+            outVelocity = vec4(v, 0.0, 1.0);
         }
     `,
 };
@@ -240,7 +260,7 @@ const vertexShader = glsl`
 
     uniform vec2 viewSize;
     uniform ivec2 clothDimensions;
-    uniform sampler2D currState;
+    uniform sampler2D positionTexture;
 
     vec2 aspectCorrectInv(vec2 v, float aspect) {
         if (aspect < 1.0) v.x /= aspect;
@@ -252,7 +272,7 @@ const vertexShader = glsl`
         float aspect = viewSize.x / viewSize.y;
 
         ivec2 coord = ivec2(gl_VertexID % clothDimensions.x, gl_VertexID / clothDimensions.x);
-        vec4 state = texelFetch(currState, coord, 0);
+        vec4 state = texelFetch(positionTexture, coord, 0);
         state.rg = aspectCorrectInv(state.rg, aspect);
 
         gl_Position = projectionMatrix * modelViewMatrix * vec4(state.rg, 0.0, 1.0);
