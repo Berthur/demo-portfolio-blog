45ff55b62a14c12adec578f5266607b7ea24ccb3
Berthur <jondenvise@gmail.com>
2025-11-26 21:26:37 +0200

    Compute genetic algorithm demo on the GPU

--- src/demos/geneticDemo.ts
+++ src/demos/geneticDemo.ts
@@ -1,6 +1,11 @@
-import { Color, MathUtils, Vector2 } from "three";
+import { BufferAttribute, BufferGeometry, Camera, Color, DynamicDrawUsage, GLSL3, MathUtils, OrthographicCamera, Points, Scene, ShaderLib, ShaderMaterial, Texture, TextureLoader, Uniform, Vector2, WebGLRenderer, WebGLRenderTarget } from "three";
 import { Demo } from "./demo";
 import { NumberSetting, Settings } from "../settings";
+import { glsl } from "../utils";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { CopyShader } from "three/examples/jsm/shaders/CopyShader";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { OutputPass } from "three/examples/jsm/postprocessing/OutputPass";
 
 interface Primitive {
     position: Vector2;
@@ -26,25 +31,35 @@ const _primitive = {
 
 export class GeneticDemo extends Demo {
     private readonly canvas: HTMLCanvasElement;
-    private readonly ctx: CanvasRenderingContext2D;
 
-    private dimensions = new Vector2(500, 500);
+    private readonly dimensions = new Vector2(500, 500);
     private n = 500;
-    private stepCount = 200;
+    private stepCount = 1;
     private mutationAggressivity = 1.0;
     private mutationProbability = 0.1;
     private primitiveOpacity = 1;
 
-    private currCollection: PrimitiveCollection;
-    private mutatingCollection: PrimitiveCollection;
+    private readonly currCollection: PrimitiveCollection;
+    private readonly mutatingCollection: PrimitiveCollection;
     private currError = 1;
 
+    private readonly renderer: WebGLRenderer;
+    private readonly scene: Scene;
+    private readonly camera: Camera;
+    private readonly points: Points;
+
+    private renderTarget: WebGLRenderTarget;
+    private diffRenderTarget: WebGLRenderTarget;
+    private composer: EffectComposer;
+    private copyPass: ShaderPass;
+    private diffPass: ShaderPass;
+    private textureLoader = new TextureLoader();
     private targetImage: ImageData;
+    private targetTexture: Texture;
 
     constructor(container: HTMLElement) {
         super(container);
         this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
         container.prepend(this.canvas);
         this.canvas.width = this.dimensions.x;
         this.canvas.height = this.dimensions.y;
@@ -71,9 +86,49 @@ export class GeneticDemo extends Demo {
             GeneticDemo.setPrimitive(this.currCollection, i, _primitive);
         }
 
+        this.scene = new Scene();
+        this.camera = new OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+        this.camera.position.set(0, 0, 1);
+        this.renderer = new WebGLRenderer({ canvas: this.canvas });
+        this.points = this.createPoints();
+        this.scene.add(this.points);
+
+        this.renderTarget = new WebGLRenderTarget(this.dimensions.x, this.dimensions.y);
+        this.diffRenderTarget = new WebGLRenderTarget(this.dimensions.x, this.dimensions.y);
+        this.composer = new EffectComposer(this.renderer, this.diffRenderTarget);
+        //this.copyPass = new ShaderPass(CopyShader);
+        //this.copyPass.renderToScreen = true;
+        this.diffPass = new ShaderPass(DiffShader, 'texture1');
+        this.composer.addPass(this.diffPass);
+        const outputPass = new OutputPass();
+        this.composer.addPass(outputPass);
+        //this.composer.addPass(this.copyPass);
+        this.composer.readBuffer = this.renderTarget;
+
         this.currError = 1.0;
 
         this.createSettings();
+    }
+
+    private createPoints(): Points {
+        const g = new BufferGeometry();
+        const posrAttribute = new BufferAttribute(new Float32Array(3 * this.n), 3, false);
+        const colorAttribute = new BufferAttribute(new Uint8Array(3 * this.n), 3, true);
+        posrAttribute.usage = DynamicDrawUsage; // TODO: Test
+        colorAttribute.usage = DynamicDrawUsage;
+        g.setAttribute('position', posrAttribute);
+        g.setAttribute('color', colorAttribute);
+        // TODO: Is 'position' attribute required?
+
+        const m = new ShaderMaterial({
+            glslVersion: GLSL3,
+            vertexShader,
+            fragmentShader,
+        });
+
+        const points = new Points(g, m);
+        points.frustumCulled = false;
+        return points;
     }
 
     private static getRandom(min: number, max: number): number {
@@ -114,23 +169,30 @@ export class GeneticDemo extends Demo {
     }
 
     private render(collection: PrimitiveCollection): void {
-        const ctx = this.ctx;
-        ctx.clearRect(0, 0, this.dimensions.x, this.dimensions.y);
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, this.dimensions.x, this.dimensions.y);
+
+        const psrAttr = this.points.geometry.attributes.position;
+        const colorAttr = this.points.geometry.attributes.color;
 
         for (let i=0; i<this.n; ++i) {
-            GeneticDemo.getPrimitive(collection, i, _primitive);
-            ctx.globalAlpha = this.primitiveOpacity;
-            ctx.fillStyle = '#' + _primitive.color.getHexString();
-            ctx.beginPath();
-            ctx.arc(
-                _primitive.position.x * this.dimensions.x, _primitive.position.y * this.dimensions.y,
-                _primitive.radius * this.dimensions.y,
-                0, 2 * Math.PI
-            );
-            ctx.fill();
+            psrAttr.array[3 * i    ] = collection.posr[3 * i    ];
+            psrAttr.array[3 * i + 1] = collection.posr[3 * i + 1];
+            psrAttr.array[3 * i + 2] = collection.posr[3 * i + 2];
+            colorAttr.array[3 * i    ] = collection.color[3 * i    ];
+            colorAttr.array[3 * i + 1] = collection.color[3 * i + 1];
+            colorAttr.array[3 * i + 2] = collection.color[3 * i + 2];
         }
+        psrAttr.needsUpdate = true;
+        colorAttr.needsUpdate = true;
+
+        // TODO: Instead of rendering twice, render to target and copy to screen
+        this.renderer.setRenderTarget(null);
+        this.renderer.render(this.scene, this.camera);
+
+        this.renderer.setRenderTarget(this.renderTarget);
+        this.renderer.render(this.scene, this.camera);
+
+        this.composer.renderToScreen = false;
+        this.composer.render();
     }
 
     private mutate(error: number): void {
@@ -143,7 +205,8 @@ export class GeneticDemo extends Demo {
             _primitive.position.y += GeneticDemo.getRandom(-1, 1) * this.mutationAggressivity * error;
             _primitive.position.clamp(ZERO_VEC, ONE_VEC); // TODO: May cause bias towards edges, is that a problem?
 
-            _primitive.radius *= 1 + GeneticDemo.getRandom(-1, 1) * this.mutationAggressivity * error;
+            const scale = GeneticDemo.getRandom(-1, 1) * this.mutationAggressivity * error;
+            _primitive.radius *= scale < 0 ? 1 + 0.5 * scale : 1 + scale;
             _primitive.radius = Math.min(_primitive.radius, 0.05);
 
             _primitive.color.r = MathUtils.clamp(_primitive.color.r + GeneticDemo.getRandom(-1, 1) * 2.0 * this.mutationAggressivity * error, 0, 1);
@@ -164,10 +227,12 @@ export class GeneticDemo extends Demo {
         canvas.height = this.dimensions.y;
         const ctx = canvas.getContext('2d');
 
+        const url = '../resources/demo/wanderer.jpeg';
+
         // @ts-ignore
         const imageData: Promise<ImageData> = await new Promise((res) => {
             const image = new Image();
-            image.src = '../resources/demo/wanderer.jpeg';
+            image.src = url;
 
             image.onload = () => {
                 ctx.drawImage(image, 0, 0);
@@ -177,17 +242,24 @@ export class GeneticDemo extends Demo {
 
         this.container.append(canvas);
 
+        const texture = await this.textureLoader.loadAsync(url);
+        this.targetTexture = texture;
+        this.diffPass.uniforms.texture2.value = texture;
+
         return imageData;
     }
 
+    private imgdata = new Uint8Array(4 * this.dimensions.x * this.dimensions.y);
     private getError(): number {
-        const imageData = this.ctx.getImageData(0, 0, this.dimensions.x, this.dimensions.y);
+        // TODO: Downsample instead
+        this.renderer.readRenderTargetPixels(this.diffRenderTarget, 0, 0, this.dimensions.x, this.dimensions.y, this.imgdata);
+
         let sum = 0;
-        for (let i=0; i<imageData.data.length; ++i) {
+        for (let i=0; i<this.imgdata.length; ++i) {
             if (i % 4 !== 3) // Ignore alpha channel
-                sum += Math.abs(imageData.data[i] - this.targetImage.data[i]) / 255;
+                sum += this.imgdata[i] / 255;
         }
-        sum /= 0.75 * imageData.data.length;
+        sum /= 0.75 * this.imgdata.length;
         return sum;
     }
 
@@ -257,3 +329,58 @@ export class GeneticDemo extends Demo {
         });
     }
 }
+
+const vertexShader = glsl`
+    precision highp float;
+
+    attribute vec3 color;
+
+    flat out vec3 vColor;
+
+    void main() {
+        gl_PointSize = position.z * 500.0; // TODO: get viewSize
+        gl_Position = vec4(2.0 * position.xy - 1.0, 0.0, 1.0);
+        vColor = color;
+    }
+`;
+
+const fragmentShader = glsl`
+    precision highp float;
+
+    flat in vec3 vColor;
+    out vec4 fragColor;
+
+    void main() {
+        vec2 relFragPos = 2.0 * (gl_PointCoord - 0.5);
+        float r = dot(relFragPos, relFragPos);
+        if (r > 1.0) discard;
+        fragColor = vec4(vColor, 1.0);
+    }
+`;
+
+const DiffShader = {
+	name: 'DiffShader',
+	uniforms: {
+		'texture1': { value: null },
+        'texture2': { value: null },
+	},
+	vertexShader: glsl`
+		varying vec2 vUv;
+
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+		}
+    `,
+	fragmentShader: glsl`
+		uniform sampler2D texture1;
+        uniform sampler2D texture2;
+		varying vec2 vUv;
+
+		void main() {
+	        vec3 color1 = texture2D(texture1, vUv).rgb;
+            vec3 color2 = texture2D(texture2, vUv).rgb;
+			gl_FragColor = vec4(abs(color1 - color2), 1.0);
+		}
+    `
+};
